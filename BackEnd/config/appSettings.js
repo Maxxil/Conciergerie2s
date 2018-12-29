83018e5c1640b3f8eaadb0bf78ff20445fc90575
nticstudio <CIRENE@lenovo-home>
2018-12-29 01:50:11 +0100

    Fix paypal problem

--- BackEnd/config/appSettings.js
+++ BackEnd/config/appSettings.js
@@ -1,5 +1,8 @@
 module.exports = {
     currentUrl : '',
-    paypalSuccess : 'http://localhost:4444/paypal/executePayment',
-    paypalCancel : 'http://localhost:4444/paypal/cancelPayment'
+    paypalSuccess : 'http://149.56.13.37:5555/paypal/executePayment',
+    paypalCancel : 'http://149.56.13.37:5555/paypal/cancelPayment'
+    //paypalSuccess : 'http://localhost:4444/paypal/executePayment',
+    //paypalCancel : 'http://localhost:4444/paypal/cancelPayment'
+    
 };
