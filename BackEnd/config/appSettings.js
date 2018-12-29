d9e5d2001d85b2b385bbb4aedfd6ddbfa565339f
nticstudio <CIRENE@lenovo-home>
2018-12-29 02:37:26 +0100

    Fix return url for paypal

--- BackEnd/config/appSettings.js
+++ BackEnd/config/appSettings.js
@@ -1,7 +1,7 @@
 module.exports = {
     currentUrl : '',
-    paypalSuccess : 'http://149.56.13.37:5555/paypal/executePayment',
-    paypalCancel : 'http://149.56.13.37:5555/paypal/cancelPayment'
+    paypalSuccess : 'http://149.56.13.37:4444/paypal/executePayment',
+    paypalCancel : 'http://149.56.13.37:4444/paypal/cancelPayment'
     //paypalSuccess : 'http://localhost:4444/paypal/executePayment',
     //paypalCancel : 'http://localhost:4444/paypal/cancelPayment'
     
