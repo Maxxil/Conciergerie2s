e7c1988b6251619435e4658084dc9dde24c2c97d
nticstudio <CIRENE@lenovo-home>
2019-01-17 22:15:08 +0100

    Fix chatserver

--- ChatServer/app.js
+++ ChatServer/app.js
@@ -88,7 +88,7 @@ io.on('connection', (socket) => {
     pushMessage.postBody["excluded_segments"] = ["Banned Users"];  
     */
 
-   pushMessage.postBody["include_player_ids"] = ['73001d0b-1fda-496e-aaf5-f6d97cff8e7ed'];
+   pushMessage.postBody["include_player_ids"] = ['73001d0b-1fda-496e-aaf5-f6d97cff8e7e'];
     console.log('PushMessage object to Admin For Chat',pushMessage);
     	
     Conciergeries2SAdmin.sendNotification(pushMessage, function (err, httpResponse,data) {      
